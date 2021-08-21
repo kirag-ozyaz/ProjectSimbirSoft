d0ab9003554f3349de14f6b81810c9c605cc0c38
Kirag <56257683+kirag-ozyaz@users.noreply.github.com>
2021-08-21 19:39:28 +0400

    !

--- WindowsFormsApp4/Properties/Resources.Designer.cs
+++ WindowsFormsApp4/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WindowsFormsApp4.Properties {
+namespace ProjectSimbirSoft.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace WindowsFormsApp4.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WindowsFormsApp4.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ProjectSimbirSoft.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
